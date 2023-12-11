d0798def284fbbdb5434b4d55e1ced20f1931ba9
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-12-10 21:04:35 -0500

    feat(fithelper-front): add theme toggle

--- libs/fithelper-front/homepage/feature/src/lib/homepage-feature.component.ts
+++ libs/fithelper-front/homepage/feature/src/lib/homepage-feature.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectThemeType, ThemeTogglerComponent } from '@fithelper/shared/theme-toggler/ui';
+import { handleAppTheme, toggleTheme } from '@fithelper/fithelper-front/homepage/util';
+
 
 @Component({
   selector: 'fithelper-homepage-feature',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>Welcome to fithelper-homepage-feature works!</p>`,
+  imports: [CommonModule, ThemeTogglerComponent],
+  template: `
+      <p>Welcome to fithelper-homepage-feature works!</p>
+      <fithelper-theme-toggler (selectTheme)="toggleTheme($event)" [label]="labelThemeToggler" [selectItems]="selectItems" />
+  `,
 })
-export class HomepageFeatureComponent {}
+export class HomepageFeatureComponent implements OnInit {
+  public readonly labelThemeToggler = 'Theme';
+  public readonly selectItems: readonly SelectThemeType[] = [
+    { label: 'Dark', value: 'night' },
+    { label: 'Light', value: 'nord' },
+    { label: 'Sydowh', value: 'synthwave' },
+    { label: 'Croucx', value: 'sunset' },
+  ]
+
+  public ngOnInit(): void {
+    handleAppTheme(this.selectItems[0].value, this.selectItems[1].value);
+  }
+
+  public toggleTheme(theme: string): void {
+    toggleTheme(theme)
+  }
+}
