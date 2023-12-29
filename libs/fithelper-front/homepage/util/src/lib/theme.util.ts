eb01c259c62aba2480db7d3a670c8387225f841e
Gilles Ferrand <gilles.ferrand31@gmail.com>
2023-12-29 18:44:02 -0500

    ci(monorepo): test github actions

--- libs/fithelper-front/homepage/util/src/lib/theme.util.ts
+++ libs/fithelper-front/homepage/util/src/lib/theme.util.ts
@@ -1,18 +1,24 @@
 import { localStorageSetItem } from '@fithelper/shared/local-storage/util';
 
-export function handleAppTheme(defaultLightTheme: string, defaultDarkTheme: string): void {
+export function handleAppTheme(
+  defaultLightTheme: string,
+  defaultDarkTheme: string
+): void {
   const theme = localStorage.getItem('theme');
   if (theme) {
     setDataTheme(theme);
     return;
   }
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  setDataTheme(defaultDarkTheme);
-  localStorageSetItem('theme', defaultDarkTheme);
-} else {
-  setDataTheme(defaultLightTheme);
-  localStorageSetItem('theme', defaultLightTheme);
-}
+  if (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  ) {
+    setDataTheme(defaultDarkTheme);
+    localStorageSetItem('theme', defaultDarkTheme);
+  } else {
+    setDataTheme(defaultLightTheme);
+    localStorageSetItem('theme', defaultLightTheme);
+  }
 }
 
 export function toggleTheme(theme: string): void {
