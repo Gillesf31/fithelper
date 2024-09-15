a5654a9382947d1d215f50284621e082ea9e5a55
Gilles Ferrand <gilles.ferrand31@gmail.com>
2024-09-15 18:58:50 -0400

    fix(fithelper-front): add env file and fix Supabase deps

--- libs/fithelper-front/supabase/data-access/supabase.service.ts
+++ libs/fithelper-front/supabase/data-access/supabase.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '@fithelper/fithelper-front/environments/util';
 
 @Injectable()
 export class SupabaseService {
   public readonly supabaseClient: SupabaseClient;
 
   constructor() {
-    // TODO: Move this to environment file
-    const supabaseUrl = '';
-    const supabaseKey = '';
+    const supabaseUrl = environment.supabaseUrl;
+    const supabaseKey = environment.supabaseKey;
     this.supabaseClient = createClient(supabaseUrl, supabaseKey);
   }
 }
