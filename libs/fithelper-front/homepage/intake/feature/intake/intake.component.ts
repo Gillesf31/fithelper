c6d2bae590761eb1f6cd2d60d02e89e314cce589
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-01-21 21:54:55 -0500

    feat(fithelper-front): use signals on intake form

--- libs/fithelper-front/homepage/intake/feature/intake/intake.component.ts
+++ libs/fithelper-front/homepage/intake/feature/intake/intake.component.ts
@@ -22,55 +22,101 @@ import { TranslocoPipe } from '@jsverse/transloco';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IntakeComponent {
-  protected defaultUserInformation: WritableSignal<UserInformationType> =
-    signal({
-      gender: 'male',
-      activityFactor: 1.4,
-      caloriesAdjustment: '250',
-      measurementSystem: 'metric',
-    });
-  protected bmr: WritableSignal<number> = signal(0);
-  protected weight: WritableSignal<number> = signal(0);
-  protected caloriesAdjustment: WritableSignal<number> = signal(0);
+  protected userInformation: WritableSignal<UserInformationType> = signal({
+    age: undefined,
+    weight: undefined,
+    gender: 'male',
+    activityFactor: 1.4,
+    caloriesAdjustment: 250,
+    measurementSystem: 'metric',
+  });
+
   readonly #intakeService = inject(IntakeService);
+
+  protected bmr = computed(() => {
+    return this.#intakeService.calculateMaintenance(this.userInformation());
+  });
+
   protected muscleGains = computed(() => {
+    if (!this.userInformation().weight) return 0;
+    if (!this.userInformation().caloriesAdjustment) return 0;
+
     return this.#intakeService.calculateCaloriesIntake(
       this.bmr(),
-      this.weight(),
-      this.caloriesAdjustment(),
+      this.userInformation().weight!,
+      this.userInformation().caloriesAdjustment!,
     );
   });
   protected loseFat = computed(() => {
+    if (!this.userInformation().weight) return 0;
+    if (!this.userInformation().caloriesAdjustment) return 0;
+
     return this.#intakeService.calculateCaloriesIntake(
       this.bmr(),
-      this.weight(),
-      -this.caloriesAdjustment(),
+      this.userInformation().weight!,
+      -this.userInformation().caloriesAdjustment!,
     );
   });
 
-  protected calculateMaintenance(userStats: UserInformationType): void {
-    if (!userStats.age || !userStats.weight || !userStats.height) return;
-    this.caloriesAdjustment.set(parseInt(userStats.caloriesAdjustment));
-    this.weight.set(userStats.weight);
-    this.bmr.set(this.#intakeService.calculateMaintenance(userStats));
-  }
-
   protected updateSystem(): void {
-    this.defaultUserInformation.set({
-      ...this.defaultUserInformation(),
+    this.userInformation.set({
+      ...this.userInformation(),
       measurementSystem:
-        this.defaultUserInformation().measurementSystem === 'metric'
+        this.userInformation().measurementSystem === 'metric'
           ? 'imperial'
           : 'metric',
+    });
+  }
+
+  protected updateGender(event: Event): void {
+    const gender = (event.target as HTMLInputElement)
+      .value as UserInformationType['gender'];
+    this.userInformation.set({
+      ...this.userInformation(),
+      gender,
     });
   }
 
   protected updateActivityFactor(event: Event): void {
     const activityFactor = (event.target as HTMLInputElement)
       .value as unknown as UserInformationType['activityFactor'];
-    this.defaultUserInformation.set({
-      ...this.defaultUserInformation(),
+    this.userInformation.set({
+      ...this.userInformation(),
       activityFactor,
+    });
+  }
+
+  protected updateCaloriesAdjustment(event: Event): void {
+    const caloriesAdjustment = (event.target as HTMLInputElement)
+      .value as unknown as UserInformationType['caloriesAdjustment'];
+    this.userInformation.set({
+      ...this.userInformation(),
+      caloriesAdjustment:
+        (Number(caloriesAdjustment) as 250 | 500 | 750) || undefined,
+    });
+  }
+
+  protected updateAge(event: Event): void {
+    const age = (event.target as HTMLInputElement).value;
+    this.userInformation.set({
+      ...this.userInformation(),
+      age: parseInt(age),
+    });
+  }
+
+  protected updateWeight(event: Event): void {
+    const weight = (event.target as HTMLInputElement).value;
+    this.userInformation.set({
+      ...this.userInformation(),
+      weight: parseInt(weight),
+    });
+  }
+
+  protected updateHeight(event: Event): void {
+    const height = (event.target as HTMLInputElement).value;
+    this.userInformation.set({
+      ...this.userInformation(),
+      height: parseInt(height),
     });
   }
 }
