b7b72ca4ff750192617332675f086ed009076d5a
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-02-01 22:28:49 -0500

    test(fithelper-front): add E2E tests for login page

--- apps/fithelper-front-e2e/src/login.spec.ts
+++ apps/fithelper-front-e2e/src/login.spec.ts
@@ -5,8 +5,12 @@ const selectors = {
   submitButton: '[data-testid="login-submit-button"]',
   emailError: '[data-testid="alert-email-pattern-error"]',
   registerLink: '[data-testid="register-link"]',
+  nonExistentAccountError: '[data-testid="error-non-existent-account"]',
+  loginSuccess: '[data-testid="login-success"]',
   header: 'h1',
 };
+
+const email = 'test@fithelper.com';
 
 test.describe('Login page', () => {
   test.beforeEach(async ({ page }) => {
@@ -41,5 +45,47 @@ test.describe('Login page', () => {
   test('should redirect', async ({ page }) => {
     await page.click(selectors.registerLink);
     await expect(page).toHaveURL('/register');
+  });
+
+  test('should display non existing account error', async ({ page }) => {
+    await page.fill(selectors.emailInput, email);
+    await page.route('*/**/auth/v1/otp', async (route) => {
+      await route.fulfill({
+        json: {
+          email,
+          data: {},
+          create_user: false,
+          gotrue_meta_security: {},
+          code_challenge: null,
+          code_challenge_method: null,
+        },
+        status: 422,
+      });
+    });
+    await page.click(selectors.submitButton);
+    await expect(page.locator(selectors.nonExistentAccountError)).toContainText(
+      `We couldn't find your account with ${email} email!`,
+    );
+  });
+
+  test('should display successfully sent magic link', async ({ page }) => {
+    await page.fill(selectors.emailInput, email);
+    await page.route('*/**/auth/v1/otp', async (route) => {
+      await route.fulfill({
+        json: {
+          email,
+          data: {},
+          create_user: false,
+          gotrue_meta_security: {},
+          code_challenge: null,
+          code_challenge_method: null,
+        },
+        status: 200,
+      });
+    });
+    await page.click(selectors.submitButton);
+    await expect(page.locator(selectors.loginSuccess)).toContainText(
+      `You can now check your email ${email} and click on the link!`,
+    );
   });
 });
